7390a13b518f8e678599ee20e8557cd6a20de349
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2022-04-17 00:32:22 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,49 @@
+document.write(typeof "Word");
+
+function my_Function() {
+    document.getElementById("Test") .innerHTML = 0/0;
+}
+
+document.write(-3E310) ;
+
+document.write(2E310) ;
+
+document.write(10>2) ;
+
+document.write(10 < 2) ;
+
+console.log(2 + 2) ;
+
+document.write("10" + 5);
+
+console.log(10 < 2) ;
+
+document.write(10 == 10) ;
+
+document.write(3 ==11) ;
+
+x = 10;
+y = 10;
+document.write(x === y) ;
+
+x = 82;
+y = "82";
+document.write(x === y) ;
+
+A = "Magnus";
+B = "Magnum";
+document.write(A === B) ;
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 >10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function() {
+    document.getElementById("Not") .innerHTML = ! (20 > 10) ;
+}
+
+
